baf1d0a90d1fdfd0811d8ec55af486b647a0d629
Ivan583 <ivanych2293@gmail.com>
2024-04-15 18:06:53 +0300

    доработка карточки

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {FC} from "react";
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
 
 interface CardProps {
     width?: string;
     height?: string;
-    children?: React.ReactChild | React.ReactNode;
+    children: React.ReactChild | React.ReactNode;
+    variant: CardVariant;    
 }
 
-const Card = ({width, height, children}: CardProps) => {
+const Card: FC<CardProps> = 
+({
+    width, 
+    height,
+    children,
+    variant,
+
+}) => {
     return (
-        <div style={{width, height, border: '2px solid teal'}}>
+        <div style={{width, height, 
+        border: variant === CardVariant.outlined ? '2px solid teal' : 'none',
+        background: variant === CardVariant.primary ? 'yellowgreen' : ''}}>
             {children}
         </div>
     )

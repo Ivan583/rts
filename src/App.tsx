710e6decfd3534e8d8e1567e3884b4ff3d071c68
Ivan583 <ivanych2293@gmail.com>
2024-04-15 17:16:05 +0300

    add content into card

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="App">
-      <Card width='200px' height='200px'/>
+      <Card width='200px' height='200px'>
+        <button>Click me</button>
+        <div>Some text</div>
+      </Card>
     </div>
   );
 }
